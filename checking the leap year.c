e2e1c28c445b07b5e8adb59b1da97f54bdabcf9c
mihirofficial3113-commits <mihir.official3113@gmail.com>
2026-03-25 00:21:30 +0530

    Add files via upload

---
+++ checking the leap year.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf ("enter is year to be checked");
+    scanf("%d",&year);
+    if (year %4==0){
+        printf("entered number is leap year");
+    }
+    else{
+        printf("entered year is not a leap year");
+
+    }
+    }
