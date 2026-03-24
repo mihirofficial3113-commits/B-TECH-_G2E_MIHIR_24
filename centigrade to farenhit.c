357f8ea947f67fdd15a33cd74bb89fe91cdf18fd
mihirofficial3113-commits <mihir.official3113@gmail.com>
2026-03-25 00:21:06 +0530

    Add files via upload

---
+++ centigrade to farenhit.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+
+float c,f;
+printf("enter the temperature in c:");
+scanf("%f",&c);
+f=32+9*c|5;
+printf("the converted temperature in f is:%f",f);
+return 0;
+
+}
