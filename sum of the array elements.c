f4bdc9d3e9bc2a35770110a6d41e0f8c6e4610db
mihirofficial3113-commits <mihir.official3113@gmail.com>
2026-04-13 20:35:04 +0530

    Add files via upload

---
+++ sum of the array elements.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+    int n, array[n],sum=0,i;
+    printf("enter the no. of elements in the array:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&array[i]);
+        {
+            for(i=0;i<n;i++);
+            {
+                sum+=array[i];
+                printf("the sum of the array is %d",sum);
+                return 0;
+
+
+            }
+        }
+    }
+}
